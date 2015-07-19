75431a6f3e43dad8c757263e5b12016bf0f501cd
Markus Fenske <iblue@gmx.net>
2015-07-19 19:03:29 +0200

    Add time measurement

--- forward.c
+++ forward.c
@@ -1,9 +1,17 @@
+#define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "swap.h"
 #include "twiddle.h"
 
 #define VERBOSE
+
+double wall_clock() {
+  struct timespec t;
+  clock_gettime(CLOCK_MONOTONIC, &t);
+  return (double)t.tv_sec + 1.0e-9*t.tv_nsec;
+}
 
 // Forward transform a given file using NTT
 int main(int argc, char *argv[]) {
@@ -12,7 +20,15 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
+  double time0 = wall_clock();
+
   ensure_twiddle(1024*1024*16);
 
+  double time1 = wall_clock();
+
   swap_ntt_forward(argv[1], 1024*1024); // 1 MB I/O buffer
+
+  double time2 = wall_clock();
+
+  printf("Twiddle Generation: %f sec, NTT: %f sec\n", time1 - time0, time2 - time1);
 }
